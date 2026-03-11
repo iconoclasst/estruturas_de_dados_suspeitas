92268904e4a22094753b671864b9348c0896d4f8
iconoclasst <davi09bezerra70@gmail.com>
2026-03-11 19:10:35 -0300

    automatic commit

---
+++ tipos_basicos.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void) 
+{
+    printf("Tipos básicos são divididos por funcionalidade e tamanho\n");
+    printf("Tipos inteiros:\na)char\nb)short int\nc)long int \n");
+    printf("Tipos reais:\na)float\nb)double \n");
+
+    
+    
+}
