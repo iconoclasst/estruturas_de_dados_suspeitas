8f3829af3dba72b8560c29947783fd12211913d2
iconoclasst <davi09bezerra70@gmail.com>
2026-03-11 20:31:53 -0300

    automatic commit

---
+++ besteira_1/break.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int i;
+    for (i=0; i < 10; i++) {
+        if (i==5)
+            break
+        printf("%d ", i);
+    }
+
+   printf("fim\n");
+
+return 0;
+}
